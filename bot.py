cdc8a40ca0a5bc2f160f3b2e62c4a9d3eafd44f0
Raiihan <70223152+raiihan@users.noreply.github.com>
2025-04-17 15:36:45 +0600

    changes async

--- bot.py
+++ bot.py
@@ -87,4 +87,12 @@ async def main():
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(main())
+
+    try:
+        asyncio.run(main())
+    except RuntimeError as e:
+        if "already running" in str(e):
+            loop = asyncio.get_event_loop()
+            loop.create_task(main())
+            loop.run_forever()
+
